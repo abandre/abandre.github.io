1ece5ff6c11a9477decbaeced9e269a25f34b2b0
abandre <amaury.andre@gmail.com>
2020-09-01 21:48:12 -0300

    aula3

---
+++ soa/_site/alomundo2.c
@@ -0,0 +1,25 @@
+#include <linux/module.h> 
+
+MODULE_LICENSE("Dual BSD/GPL"); 
+
+static int numero;
+module_param(numero, int, 0);
+
+static char *texto = "Ola!";
+module_param(texto, charp, 0);
+
+static int alo_inicio(void) {
+	int i;
+	for (i=0;i<numero;i++) {
+		printk(texto);
+	}  
+    return 0; 
+} 
+
+static void alo_fim(void) { 
+    printk("Adeus, Mundo Cruel!\n"); 
+} 
+
+
+module_init(alo_inicio); 
+module_exit(alo_fim);
